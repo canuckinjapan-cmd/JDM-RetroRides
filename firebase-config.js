20936c86068820af07e116d658686fde509d324e
Dan Burgess <canuck.in.japan@gmail.com>
2026-04-18 16:24:24 +0900

    Fix: Hardcode Firebase config for GitHub Pages deployment

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export default {
-  "projectId": import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  "appId": import.meta.env.VITE_FIREBASE_APP_ID,
-  "apiKey": import.meta.env.VITE_FIREBASE_API_KEY,
-  "authDomain": import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  "storageBucket": import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  "messagingSenderId": import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  "firestoreDatabaseId": import.meta.env.VITE_FIREBASE_DATABASE_ID,
+  "projectId": "gen-lang-client-0048174914",
+  "appId": "1:508863999083:web:c47694ff3f044edc14952e",
+  "apiKey": "AIzaSyBh80Waj6XBpwwOp4dbc_1uiwNOl0vC4v8",
+  "authDomain": "gen-lang-client-0048174914.firebaseapp.com",
+  "storageBucket": "gen-lang-client-0048174914.firebasestorage.app",
+  "messagingSenderId": "508863999083",
+  "firestoreDatabaseId": "ai-studio-ce06ebb3-6b6d-4772-a340-73bd224515f7",
   "measurementId": ""
 };
